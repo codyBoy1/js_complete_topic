39f45caddcd005133d59e3c6a584e72d42cc50f6
cody <rv502887@gmail.com>
2024-03-23 21:25:39 +0530

    23march

--- oop/oop.js
+++ oop/oop.js
@@ -1,5 +1,35 @@
-const obj ={
+const user ={
   username :"amit",
   userEmail:"amit@example.com",
-  userPassword :1234
+  userPassword :1234,
+  getAllUser : function (){
+    console.log("this is a function")
+    console.log(this.username)
+   
+  }
 }
+
+//console.log(user.username)
+//console.log(user.getAllUser())
+
+function User(username,loginCount,isLoggedIn){
+  this.username=username
+  this.loginCount=loginCount
+  this.isLoggedIn=isLoggedIn
+  //return this 
+}
+//const userOne =User("amit",10,true)
+//const userTwo =User("amit vishwakarma",11,false)
+// this will overwrite value of userOne 
+// so to get rid of this use new keyword
+
+//console.log(userOne)
+
+
+//const userOne = new User("amit",10,true)
+const userTwo =new User("amit vishwakarma",11,false)
+//console.log(userOne)
+//console.log(userTwo)
+
+
+//console.log(userTwo.constructor)
---
+++ oop/prototype.js
@@ -0,0 +1,10 @@
+function multiplyBy5(num){
+  return num*5
+}
+
+multiplyBy5.Name ="amit"
+console.log(multiplyBy5(6))
+console.log(multiplyBy5.Name)
+console.log(multiplyBy5.prototype)
+
+//function can also be treated as object 
